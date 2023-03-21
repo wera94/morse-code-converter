baa177f8f9c0ca10c65dbbd80f8eedc611a9c864
Weronika Maziarz <weronika.maziarz@interia.eu>
2023-03-21 18:53:43 +0100

    init commit

---
+++ src/main/java/pl/sdacademy/converter/Main.java
@@ -0,0 +1,9 @@
+package pl.sdacademy.converter;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ src/test/java/pl/sdacademy/converter/MainTest.java
@@ -0,0 +1,4 @@
+package pl.sdacademy.converter;
+
+public class MainTest {
+}
