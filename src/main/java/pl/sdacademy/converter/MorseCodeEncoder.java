7af2123f6fb83c1c2d6df7e7e55e859af6d1624e
Weronika Maziarz <weronika.maziarz@interia.eu>
2023-03-21 19:59:14 +0100

    Task 2 - MorseCodeDecoder and MorseCodeEncoder added

---
+++ src/main/java/pl/sdacademy/converter/MorseCodeEncoder.java
@@ -0,0 +1,17 @@
+package pl.sdacademy.converter;
+
+public class MorseCodeEncoder {
+    private final MorseCodeDictionary dictionary;
+
+
+    public MorseCodeEncoder(MorseCodeDictionary dictionary) {
+        this.dictionary = dictionary;
+    }
+
+
+    public String encode(String text) {
+        return "";
+    }
+
+
+}
